2ea3ee9ea0ff5868bd72d402f11026e1239e4a15
lalbari <lalbari98@gmail.com>
2025-01-06 09:27:50 +0100

    Primera Página de Proyecto con Locomotive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-/* trabajados */
+/* Trabajadores */
 function showOverlay(container) {
        
     if (container.classList.contains('active')) {
@@ -12,3 +12,23 @@ function showOverlay(container) {
       container.classList.add('active');
     }
   }
+
+
+/* Locomotive */
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#body"),
+    smooth: true,
+    lerp: 0.1
+});
+
+  const image = document.querySelector('.scroll-image');
+
+  scroll.on('scroll', function () {
+      const scrollPosition = scroll.scroll.instance.scroll.y;
+      const scale = Math.max(1 - (scrollPosition / 1000), 0.5);
+      image.style.transform = `scale(${scale})`;
+  });
+
+window.addEventListener('load', () => {
+    scroll.update();
+});
