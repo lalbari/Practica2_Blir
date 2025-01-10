641369d5e622c34d471d252b2aff0efd746aa048
lalbari <lalbari98@gmail.com>
2025-01-10 21:26:10 +0100

    arreglos nav, locomotive, responsive y peso imagenes

--- js/scriptlocomotive.js
+++ js/scriptlocomotive.js
@@ -1,17 +1,29 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector("#body"),
-    smooth: true,
-    lerp: 0.1
-});
+function initScroll() {
+    if (window.innerWidth > 768) {
+        const scroll = new LocomotiveScroll({
+            el: document.querySelector("#main"),
+            smooth: true,
+            lerp: 0.1,
+        });
 
-  const image = document.querySelector('.scroll-image');
+        const image = document.querySelector('.scroll-image');
 
-  scroll.on('scroll', function () {
-      const scrollPosition = scroll.scroll.instance.scroll.y;
-      const scale = Math.max(1 - (scrollPosition / 1000), 0.5);
-      image.style.transform = `scale(${scale})`;
-  });
+        scroll.on('scroll', function () {
+            const scrollPosition = scroll.scroll.instance.scroll.y;
+            const scale = Math.max(1 - (scrollPosition / 1000), 0.5);
+            image.style.transform = `scale(${scale})`;
+        });
 
-window.addEventListener('load', () => {
-    scroll.update();
+        window.addEventListener('load', () => {
+            scroll.update();
+        });
+    } else {
+        console.log("LocomotiveScroll deshabilitado en pantallas pequeñas");
+    }
+}
+
+initScroll();
+
+window.addEventListener('resize', () => {
+    initScroll();
 });
